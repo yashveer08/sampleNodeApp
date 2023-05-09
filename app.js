3e1749e256adef2633da738d1ebcb9b77734b1a4
yashveer08 <yashveer.bhadouria@bugendaitech.com>
2023-05-09 19:41:02 +0530

    added some files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
+    console.log("Server started!!");
     res.json({ message: "Welcome to Sample Node application" });
 });
 app.listen(3000, function () {
